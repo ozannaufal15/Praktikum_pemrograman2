dd524b7fe61f69d29745056d4fab57b12a0d1c93
ozannaufal15 <ozan.naufal15@gmail.com>
2022-10-12 16:20:09 +0800

    .

--- PRAK2/PRAK201_2110817310005_MuhammadFauzanNaufalRidho/Soal1Main.java
+++ PRAK2/PRAK201_2110817310005_MuhammadFauzanNaufalRidho/Soal1Main.java
@@ -3,10 +3,10 @@ package PRAK201_2110817310005_MuhammadFauzanNaufalRidho;
 public class Soal1Main {
 
     public static void main(String[] args) {
-        Mangga arummanis=new Mangga("Arummanis",0.3f,5000,13);
+        Mangga arumanis=new Mangga("Arumanis",0.3f,5000,13);
         Mangga manalagi=new Mangga("Manalagi",0.5f,7500,17);
-        Mangga manggaMadu=new Mangga("Arummanis",0.375f,6500,12);
-        arummanis.printDetail();
+        Mangga manggaMadu=new Mangga("Mangga Madu",0.375f,6500,12);
+        arumanis.printDetail();
         manalagi.printDetail();
         manggaMadu.printDetail();
     }
